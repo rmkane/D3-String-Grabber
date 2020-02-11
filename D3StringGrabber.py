8376f61b4c786c69b9109856771bf2210b609514
ryankane <rmkane89@gmail.com>
2020-02-11 15:36:55 -0500

    Added support for more files

--- D3StringGrabber.py
+++ D3StringGrabber.py
@@ -1,25 +1,33 @@
+import re
 import struct
 from collections import namedtuple
 
-class stlFile:
-	sizeDesc = 0x50
-	sizeHeader = 0x38
-	sizeMPQHeader = 0x10
-	sizeRecord = 0x10
-	def init(self, filePath):
+class StlFile:
+	sizeDesc      = 0x28 # Default description size is 40 bytes (sometimes 80 bytes)
+	sizeHeader    = 0x28 # Default header size is 40 bytes
+	sizeMPQHeader = 0x10 # Default MPQ header size is 16 bytes
+	sizeRecord    = 0x10 # Default record size is 16 bytes
+
+	def __init__(self, filePath, recordCount, scaleDesc=1.0, sortResult=False):
+		self.recordCount = recordCount
+		self.sizeDesc = int(self.sizeDesc * scaleDesc) # Sometimes this is 2x the size
+		self.sortResult = sortResult
+		
 		try:
 			self.fileHandle = open(filePath, 'rb')
 		except IOError:
-			print 'stlFile: That .stl file does not exist or is read protected'
+			print('StlFile: That .stl file does not exist or is read protected')
 		else:
 			if not self.readHeader():
-				print 'Error: this is not a correct STL file!'
+				print('Error: this is not a correct STL file!')
 			else:
 				self.readStrings()
+		self.fileHandle.close()
 				
 	def readHeader(self):
 		stlHeader = namedtuple('stlHeader', 'magic unk1 free1 unk2 free2 free3 free4 unk3 dataStart free5')
 		self.header = stlHeader(*struct.unpack('@IILIILLIIL', self.fileHandle.read(self.sizeHeader)))
+		#print(self.header)
 		if (not self.header.magic == 0xDEADBEEF):
 			return False
 		return True
@@ -27,14 +35,15 @@ class stlFile:
 	def readStrings(self):
 		stlRecord = namedtuple('stlRecord', 'type free1 start length')
 		stlDesc = namedtuple('stlDesc', 'name value add1 add2 end')
-		numOfRecords = (self.header.dataStart - self.sizeHeader) / self.sizeDesc
+		numOfRecords = (self.header.dataStart - self.sizeHeader) // self.sizeDesc
+		#print(self.header.dataStart, self.sizeHeader)
 		self.desc = []
 		self.stringList = []
-		for i in range(numOfRecords):
+		for i in range(self.recordCount):
 			desc = []
 			string = []
 			for j in range(5):
-				self.fileHandle.seek(i * self.sizeDesc + self.sizeHeader + j * self.sizeRecord)	
+				self.fileHandle.seek((i * self.sizeDesc) + self.sizeHeader + j * self.sizeRecord)	
 				record = stlRecord(*struct.unpack('@IIII', self.fileHandle.read(self.sizeRecord)))
 				self.fileHandle.seek(record.start + self.sizeMPQHeader)
 				string.append(self.fileHandle.read(record.length))
@@ -42,3 +51,20 @@ class stlFile:
 			sDesc = stlDesc(desc[0], desc[1], desc[2], desc[3], desc[4])
 			self.desc.append(sDesc)
 			self.stringList.append(string)
+		if self.sortResult:
+			self.stringList = sorted(self.stringList, key=lambda string: self.decodeValue(string[1]).lower()) # Sort by key
+
+	def printList(self):
+		for string in self.stringList:
+			print(self.decodeValue(string[1]) + ' => ' + self.decodeValue(string[2]))
+
+	def writeToFile(self, filename):
+		with open(filename, 'w') as f:
+			for string in self.stringList:
+				f.write("{0}\t{1}\n".format(self.decodeValue(string[1]), self.decodeValue(string[2])))
+
+	def decodeValue(self, unicodeValue):
+		return self.trim(unicodeValue.decode('UTF-8').strip())
+
+	def trim(self, stringValue):
+		return re.sub(r'^\W*|\W*$', "", stringValue, flags=re.UNICODE)
--- driver.py
+++ driver.py
@@ -1,12 +1,28 @@
-from D3StringGrabber import stlFile
+import os
+from D3StringGrabber import StlFile
 
-D3Affixes = stlFile()
-D3Affixes.init('Affixes.stl')
-"""
-for desc in D3Affixes.desc:
-	print '\nDesc'
-	for record in desc:
-		print str(record)
-"""
-for string in D3Affixes.stringList:
-	print string
+DIR_INPUT = 'input'
+DIR_OUTPUT = 'output'
+
+FILE_ARGS = [
+	('Affixes', 2208, 2.0),
+	('Bnet_Store', 300),
+	('BnetStore_ItemDescriptions', 293),
+	('General', 285),
+	('Items', 6242),
+]
+
+def setup():
+	os.mkdir(DIR_OUTPUT) if not os.path.exists else None
+
+def run():
+	[parse(*args) for args in FILE_ARGS]
+
+def parse(filename, lines, scale=1.0, sortResults=True):
+	stl = StlFile('{0}/{1}.stl'.format(DIR_INPUT, filename), lines, scale, sortResults)
+	stl.writeToFile('{0}/{1}.txt'.format(DIR_OUTPUT, filename))
+	print('Parsed file: {0}.stl'.format(filename))
+
+if __name__ == '__main__':
+	setup()
+	run()
